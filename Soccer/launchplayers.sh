4adaf7be1b9099527747b3aff4a00baeb2b812eb
calonso02 <candido.16.alonso@gmail.com>
2025-12-04 15:44:27 +0100

    Modificacion launcher

--- Soccer/launchplayers.sh
+++ Soccer/launchplayers.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-cd player/build && make
-sleep 3;
+TEAM_NAME="$1"
+PORT_START="$2"
+
+# Validación de parámetros
+if [ -z "$TEAM_NAME" ] || [ -z "$PORT_START" ]; then
+  echo "Uso: $0 <nombre_equipo> <puerto_inicial>"
+  exit 1
+fi
+
+cd build/ && make
+sleep 3
 
 for i in {0..10}
 do
-  gnome-terminal -- bash -c "~/Projects/SoccerCup/player/build/player Oesia 505$i" &
+  PORT=$((PORT_START + i))
+  gnome-terminal -- bash -c "./player $TEAM_NAME $PORT" &
   sleep 1
 done
